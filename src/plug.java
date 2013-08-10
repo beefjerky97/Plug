ab8e65966098f856c46bbc90660ab786596b5057
Sean <seanjr@baystateit.com>
2013-08-10 15:48:40 -0400

    First Creation

---
+++ src/plug.java
@@ -0,0 +1,19 @@
+//TODO Package declaration
+ 
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+public class plug extends JavaPlugin{
+   
+    public void onEnable(){
+       
+        //TODO initialize plugin
+       
+    }
+   
+    public void onDisable(){
+       
+        //TODO disable
+       
+    }
+ 
+}
